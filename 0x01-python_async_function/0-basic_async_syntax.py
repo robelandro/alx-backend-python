527dc038c70f2593a0efee636fc7fa571a890ff6
robelandro <robelpopandro@gmail.com>
2023-04-03 12:14:22 -0200

    Solve: added function that wait for random sec

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Module that wait for random sec"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    "Wait for a random amount of time, and return the amount of time waited."
+
+    The function is asynchronous, so it returns a coroutine
+
+    :param max_delay: The maximum delay in seconds, defaults to 10
+    :type max_delay: int (optional)
+    :return: A coroutine object.
+    """
+    random_float = random.uniform(0, max_delay)
+    task = asyncio.sleep(random_float)
+    await task
+    return random_float
