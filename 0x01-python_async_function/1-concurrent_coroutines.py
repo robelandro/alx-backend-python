782b2f701d219003be118db67a71586134492284
robelandro <robelpopandro@gmail.com>
2023-04-03 17:05:31 -0200

    fix: annotaion fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,7 +5,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     "Wait for n random delays, and return a list of the delays."
 
