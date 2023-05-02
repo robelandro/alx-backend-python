1e197e5b5c81481c2ffdb883a0eed899990db66b
robelpop <robelpopandro@gmail.com>
2023-05-02 17:06:26 -0200

    Solve: Implement an #Addvanced

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -104,3 +104,17 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         Stops the patcher that had been started
         """
         cls.get_patcher.stop()
+
+    def test_public_repos(self):
+        """
+        Test public_repos method without license
+        """
+        self.assertEqual(self.client.public_repos(), self.expected_repos)
+
+    def test_public_repos_with_license(self):
+        """
+        Test public_repos method with license
+        """
+        self.assertEqual(
+            self.client.public_repos(license="apache-2.0"),
+            self.apache2_repos)
